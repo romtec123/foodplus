23638c792ad9a81c41d727ef44c723b54afb35da
romtec123 <julianlevin11@gmail.com>
2020-05-29 09:37:17 -0700

    Add files via upload

---
+++ lua/entities/fp_burger/init.lua
@@ -0,0 +1,23 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+function ENT:Initialize()
+	self:SetModel("models/food/burger.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+
+	local phys = self:GetPhysicsObject()
+
+	if phys:IsValid() then
+		phys:Wake()
+	end
+end
+
+function ENT:Use(ply)
+	if ply:Health() == ply:GetMaxHealth() then return end 
+	ply:SetHealth(ply:Health() + 5)
+	ply:EmitSound(Sound("foodplus/nom".. math.random(1,5) .. ".wav"), 100, math.random(70,130))
+	if ply:Health() > ply:GetMaxHealth() then ply:SetHealth(ply:GetMaxHealth()) end
+	self:Remove()
+end
---
+++ lua/entities/fp_milk/init.lua
@@ -0,0 +1,23 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+function ENT:Initialize()
+	self:SetModel("models/props_junk/garbage_milkcarton001a.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+
+	local phys = self:GetPhysicsObject()
+
+	if phys:IsValid() then
+		phys:Wake()
+	end
+end
+
+function ENT:Use(ply)
+	if ply:Health() >= ply:GetMaxHealth() then return end 
+	ply:SetHealth(ply:Health() + 2)
+	ply:EmitSound(Sound("foodplus/drink.wav"), 100, math.random(70,130))
+	if ply:Health() > ply:GetMaxHealth() then ply:SetHealth(ply:GetMaxHealth()) end
+	self:Remove()
+end
